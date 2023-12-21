c83279ab896e0c691ca39e9a98dd343385c74f7c
Floyd Corbin <floyd.corbin@hoodoo.digital>
2023-12-20 17:18:21 -0700

    Updated content

--- logic.js
+++ logic.js
@@ -1,1 +1,7 @@
 
+   //capture the scroll event on the page and hide the image with the id of "logo" when the scroll position is greater than 5px
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > 5) { 
+            $('#logo').fadeOut();
+        }
+    });
