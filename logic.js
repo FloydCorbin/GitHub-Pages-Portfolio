7cd1ea3a32f28c60af39ffec48ee8fd6331ecb2a
Floyd Corbin <floyd.corbin@hoodoo.digital>
2023-12-21 17:55:12 -0700

    removed carousel, added grid items and additional content. Sorted scroll behavior.

--- logic.js
+++ logic.js
@@ -1,23 +1,6 @@
-
-   //capture the scroll event on the page and hide the image with the id of "logo" when the scroll position is greater than 5px
-    $(window).scroll(function() {
-        if ($(document).scrollTop() > 5) { 
-            $('#logo').fadeOut();
-        }
-    });
-
-    document.addEventListener('DOMContentLoaded', function () {
-        var carousel = new bootstrap.Carousel(document.getElementById('myCarousel'));
-
-        // Ensure focus management for keyboard navigation
-        document.getElementById('myCarousel').addEventListener('keydown', function (e) {
-            if (e.key === 'ArrowRight' || e.key === 'ArrowLeft') {
-                carousel.pause();
-            }
-        });
-
-        // Reset focus when transitioning between slides
-        document.getElementById('myCarousel').addEventListener('slide.bs.carousel', function () {
-            carousel.cycle();
-        });
-    });
+//capture the scroll event on the page and hide the image with the id of "logo" when the scroll position is greater than 5px
+$(window).scroll(function () {
+  if ($(document).scrollTop() > 30) {
+    $(".header").fadeOut();
+  }
+});
